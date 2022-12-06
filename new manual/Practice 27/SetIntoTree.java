8c66cfc87c3064b1955cd34fdb55f213f83f62fa
Kalinin Aleksandr <100154931+Agar1us@users.noreply.github.com>
2022-12-06 15:12:09 +0300

    Add files via upload

---
+++ new manual/Practice 27/SetIntoTree.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SetIntoTree {
+    public static <T> Set<T> getInstance(Set<T> hashSet) {
+        return new TreeSet<T>(hashSet);
+    }
+
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet<>();
+        hashSet.add("RED");
+        hashSet.add("BLUE");
+        hashSet.add("GREEN");
+        Set<String> treeSet = getInstance(hashSet);
+        System.out.println(treeSet);
+    }
+}
